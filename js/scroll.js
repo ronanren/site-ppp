408295052a986f83715d0817691363633f90429d
ronanren <renouxpro@gmail.com>
2019-12-17 15:07:54 +0100

    update

--- js/scroll.js
+++ js/scroll.js
@@ -1,6 +1,16 @@
 $(document).ready(function() {
-	$('#pagepiling').pagepiling({
+    $('#pagepiling').pagepiling({
         sectionsColor: ['#fff', 'blue', '#fff', 'blue', '#fff'],
         scrollingSpeed: 300
     });
 });
+
+window.onscroll = function() { myFunction() };
+
+function myFunction() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
+    
+}
