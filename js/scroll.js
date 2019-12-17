e0da7e00a6e746f6aa93b21ec8f4b809326f4504
mboultoureau <contact@mboultoureau.com>
2019-12-17 11:40:18 +0100

    Ajout du scroll avec PagePilling

--- js/scroll.js
+++ js/scroll.js
@@ -1,18 +1,6 @@
-var options = {
-    root: null,
-    rootMargin: '0px',
-    threshod: 0.25
-};
-
-
-
-var observer = new IntersectionObserver(function(entries, observer) {
-    entries.forEach(entry => {
-        console.log(entry.intersectionRatio)
-    })
-}, options);
-
-window.addEventListener("load", function(event) {
-    var target = document.querySelector("#observer");
-    observer.observe(target);
-}, false)
+$(document).ready(function() {
+	$('#pagepiling').pagepiling({
+        sectionsColor: ['#fff', 'blue', '#fff', 'blue', '#fff'],
+        scrollingSpeed: 300
+    });
+});
