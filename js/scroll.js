2b10513c262d92691fc7a1d04a9b937264f3d358
mboultoureau <contact@mboultoureau.com>
2019-12-18 00:35:44 +0100

    Ajout du menu

--- js/scroll.js
+++ js/scroll.js
@@ -1,24 +1,18 @@
-$(document).ready(function() {
-	$('#pagepiling').pagepiling({
+$(document).ready(function () {
+    $('#pagepiling').pagepiling({
         sectionsColor: ['#fff', '#0054fc', '#fff', '#0054fc', '#fff'],
         scrollingSpeed: 300,
         navigation: {
             'textColor': '#000', // modifier couleur selon le fond
             'bulletsColor': '#000',
             'position': 'right',
-            'tooltips': [document.title, document.querySelector("h3").textContent, document.querySelectorAll("h3")[1].textContent, 
-            			document.querySelectorAll("h3")[2].textContent]}
-            // tableau avec chaque h3 puis un for each
+            'tooltips': [document.title, document.querySelector("h3").textContent, document.querySelectorAll("h3")[1].textContent,
+                document.querySelectorAll("h3")[2].textContent
+            ]
+        },
+        // tableau avec chaque h3 puis un for each
+        afterLoad: function (anchorLink, index) {
+            document.getElementById("myBar").style.width = (index * 100 / 4) + "%";
+        }
     });
 });
-
-window.onscroll = function() { myFunction() };
-
-function myFunction() {
-    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    var scrolled = (winScroll / height) * 100;
-    document.getElementById("myBar").style.width = scrolled + "%";
-    
-}
-
