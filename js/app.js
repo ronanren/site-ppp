c3943ba0b14f60f637d2c4d9b1c3a8bd9aefa97f
mboultoureau <contact@mboultoureau.com>
2020-01-17 22:35:12 +0100

    Bouton Scroll To Top

--- js/app.js
+++ js/app.js
@@ -1,54 +1,18 @@
-let observer = null;
-const spies = document.querySelectorAll('header, article');
-
-const activate = function(element) {
-    const id = element.getAttribute('id')
-    const anchor = document.querySelector(`a[href="#${id}"]`)
-    if (anchor === null)
-    {
-        return null
-    }
-
-    anchor.parent
-        .querySelectorAll('.active')
-        .forEach(elem => elem.classList.remove('active'))
-    anchor.classList.add('active')
-}
-
-const callback = function(entries, observer) {
-    entries.forEach(function(entry) {
-        if (entry.intersectionRatio > 0) {
-            activate(entry.target)
-        }
-    })
-}
-
-const observe = function(elems) {
-    if (observer !== null)
-    {
-        elems.forEach(elem => observer.unobserve(elem))
-    }
-    const y = Math.round(window.innerHeight * 0.6)
-    observer = new IntersectionObserver(callback, {
-        rootMargin: `-${window.innerHeight - y - 1}px 0px -${y}px 0px`
-    })
-    elems.forEach(spy => observer.observe(spy))
-}
-
-if (spies.length > 0)
-{
-    observe(spies)
-    window.addEventListener('resize', function() {
-        observe(spies)
-    })
-}
-
 /* SCROLL INDICATOR */
 window.onscroll = function() {
     var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
     var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
     var scrolled = (winScroll / height) * 100;
     document.getElementById("scroll-indicator").style.width = scrolled + "%";
+
+
+    /* SCROLL TO TOP */
+    var scrollButton = document.querySelector('.scroll-to-top');
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        scrollButton.style.opacity = "1";
+    } else {
+        scrollButton.style.opacity = "0";
+    }
 }
 
 /* ENABLE MENU MOBILE */
