9ed945c4b949b853e9db48b4188c5bd497a1e70d
mboultoureau <contact@mboultoureau.com>
2020-01-17 22:13:38 +0100

    Scroll behavior changed

--- js/app.js
+++ js/app.js
@@ -88,6 +88,10 @@ window.onresize = function() {
 const toggleSwitch = document.querySelector('.checkbox input[type="checkbox"]');
 const currentTheme = localStorage.getItem('theme');
 
+if (!window.localStorage) {
+    currentTheme = 'light';
+}
+
 if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
   
@@ -101,8 +105,9 @@ function switchTheme(e) {
         document.documentElement.setAttribute('data-theme', 'dark');
         localStorage.setItem('theme', 'dark');
     }
-    else {        document.documentElement.setAttribute('data-theme', 'light');
-          localStorage.setItem('theme', 'light');
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
     }    
 }
 
